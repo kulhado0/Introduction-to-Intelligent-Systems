675cbc537728a582d183aa79de8b25fa9af5ffe8
João Paulo Mendonça <51032660+kulhado0@users.noreply.github.com>
2020-11-08 11:44:59 -0300

    Trabalho Finalizado

--- Trabalho 2 - Filtragem de Dados/Source/Main.java
+++ Trabalho 2 - Filtragem de Dados/Source/Main.java
@@ -45,10 +45,12 @@ class Main {
         Arq.close();
 
 
+        // Organiza por data
+
         for (int i = 0; i < numArq - 1; i++){
             int menor = i;
             for (int j = i + 1; j < numArq; j++){
-                if (vet[menor].Number > vet[j].Number){
+                if ((vet[menor].Ano >= vet[j].Ano) && (vet[menor].Mes >= vet[j].Mes) && (vet[menor].Dia >= vet[j].Dia)){
                     menor = j;
                 }
             }
@@ -75,64 +77,29 @@ class Main {
             vet[menor].Ano = auxA;
         }
 
-        for (int i = 0; i < numArq - 1; i++){
-            int menor = i;
-            for (int j = i + 1; j < numArq; j++){
-                if(vet[menor].Number == vet[j].Number){
-                    if ( ((vet[menor].Ano >= vet[j].Ano) && (vet[menor].Mes >= vet[j].Mes) && (vet[menor].Dia >= vet[j].Dia))){
-                        menor = j;
-                    }
-                }
-            }
-            int auxN = vet[i].Number;
-            int auxDia = vet[i].Dia;
-            int auxM = vet[i].Mes;
-            int auxA = vet[i].Ano;
-            String auxD = vet[i].Date;
-            String auxI = vet[i].Item;
-
-            vet[i].Number = vet[menor].Number;
-            vet[i].Date = vet[menor].Date;
-            vet[i].Dia = vet[menor].Dia;
-            vet[i].Mes = vet[menor].Mes;
-            vet[i].Ano = vet[menor].Ano;
-            vet[i].Item = vet[menor].Item;
-
-            vet[menor].Number = auxN;
-            vet[menor].Date = auxD;
-            vet[menor].Item = auxI;
-            vet[menor].Dia = auxDia;
-            vet[menor].Mes = auxM;
-            vet[menor].Ano = auxA;
-        }
-
-        // for (i = 0; i < numArq; i++){
-        //     MyIO.println(vet[i].Number +","+ vet[i].Date +","+ vet[i].Item);
-        // }
-
-        //+ vet[i].Dia +","+ vet[i].Mes +","+ vet[i].Ano +","
-
         Arq.openWrite("../Datasets/Groceries_Finished.csv");
         for(int i = 0; i < numArq; i++){
+            boolean isRepetido = false;
             String Products = vet[i].Item+",";
             for (int j = i + 1; j < numArq; j++){
-                if (vet[i].Date.compareTo(vet[j].Date) == 0){
-                    if (vet[i].Number == vet[j].Number){
-                        Products += (vet[j].Item + ",");
+                for (int k = 0; k < i; k++){
+                    if(vet[k].Number == vet[i].Number && vet[k].Date.compareTo(vet[i].Date) == 0 ){
+                        isRepetido = true;
                     }
+                }
+
+                if (vet[i].Number == vet[j].Number && vet[i].Date.compareTo(vet[j].Date) == 0){
+                    Products += (vet[j].Item + ",");
                 }
             }
 
             char adapta[] = Products.toCharArray();
-            int tam = Products.length() - 1;
-            adapta[tam] = ' ';
-            Products = new String (adapta);
+            adapta[Products.length() - 1] = ' ';
+            Products = new String(adapta);
 
-            Arq.println(vet[i].Number +","+ vet[i].Date +","+ Products);
+            if(isRepetido == false)
+                Arq.println(vet[i].Number +";"+ vet[i].Date +";"+ Products);
         }
-        // for (i = 0; i < numArq; i++){
-        //     Arq.println(vet[i].Number +","+ vet[i].Date +","+ Products);
-        // }
         Arq.close();
     }
 }
